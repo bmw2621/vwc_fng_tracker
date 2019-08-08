4bd356028817993f3a496c708ada4215ede483fb
Eddie Prislac <eddie@vetswhocode.io>
2019-08-08 00:20:40 -0500

    Fixed stupid-ass datepicker

--- vwc_fng_tracker_fe/src/components/DatePicker/date-picker.tsx
+++ vwc_fng_tracker_fe/src/components/DatePicker/date-picker.tsx
@@ -2,33 +2,34 @@ import React, { useEffect, useState } from 'react'
 import { KeyboardDatePicker } from '@material-ui/pickers'
 import moment from 'moment'
 
+
+
 export const DatePicker = (props) => {
-  const [state, setState] = useState({value: props.value});
+  const [state, setState] = useState({value: props.value, changing: false});
+  const inputRef: any = React.createRef()
+
+  const customInputRender = (props, ref) => (<input type="hidden" ref={ ref } id={ props.id } value={state.value} onClick={ props.onClick } />)
+  const CustomInput = React.forwardRef(customInputRender)
 
   const handleDateChange = (date) => {
-    setState({value: date.format('YYYY-MM-DD')})
-    const hid = document.getElementById(props.id)
+    setState({value: date.format('YYYY-MM-DD'), changing: true})
   }
+
+  useEffect(() => {
+    if (state.changing) {
+      inputRef.current.click()
+      state.changing = false
+    }
+  })
 
   const handleChange = (event) => {
     console.log(event.target.value)
     props.changeHandler(event)
   }
 
-  const styles= {
-    display: 'inline',
-    width: 0,
-    height: 0,
-    padding: 0,
-    margin: 0,
-    overflow: 'hidden'
-  }
-
   return (
     <span>
-      <div style={styles}>
-        <input type="text" style={styles} value={state.value} id={props.id} onChange={ handleChange } />
-      </div>
+      <CustomInput value={state.value} id={props.id} onClick={ handleChange } ref={inputRef} />
       <KeyboardDatePicker
         required
         autoOk
